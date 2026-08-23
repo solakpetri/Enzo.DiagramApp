3b1580bea7cc5e4e8364d8e065678c5f3793a84b
solakpetri <fatihcesur2010@gmail.com>
2026-08-23 19:25:16 +0200

    feat: initial setup

---
+++ tests/Enzo.Diagrams.Api.Tests/BootstrapTests.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Enzo.Diagrams.Api.Tests;
+
+public sealed class BootstrapTests
+{
+    [Fact]
+    public void ProjectBuilds()
+    {
+    }
+}
---
+++ tests/Enzo.Diagrams.Cli.Tests/BootstrapTests.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Enzo.Diagrams.Cli.Tests;
+
+public sealed class BootstrapTests
+{
+    [Fact]
+    public void ProjectBuilds()
+    {
+    }
+}
